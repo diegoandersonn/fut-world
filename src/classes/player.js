392450c8eb9e3bf4e7cb225e18584a4d17eef26d
diegoandersonn <diegoandersoncarva@hotmail.com>
2024-09-28 17:44:37 -0300

    Now class CreatePlayer has a method to calculate players overall

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,18 +1,34 @@
-// eslint-disable-next-line no-unused-vars
 export default class CreatePlayer {
-    constructor(name, team, nationality, age, number, position, overall, pace, shooting, passing, dribbling, defense, physical) {
+    overall;
+    constructor(name, team, nationality, age, number, position, pace, shooting, passing, dribbling, defense, physical) {
       this.name = name;
       this.team = team;
       this.nationality = nationality;
       this.age = age;
       this.number = number;
       this.position = position;
-      this.overall = overall;
       this.pace = pace;
       this.shooting = shooting;
       this.passing = passing;
       this.dribbling = dribbling;
       this.defense = defense;
       this.physical = physical;
+    }
+    getOverall() {
+      if (this.position === 'Zagueiro') {
+        return (this.defense * 0.6 + this.passing * 0.1 + this.physical * 0.3).toFixed(0);
+      } else if (this.position === 'Lateral') {
+        return (this.pace * 0.3 + this.passing * 0.2 + this.defense * 0.3 + this.dribbling * 0.2).toFixed(0);
+      } else if (this.position === 'Volante') {
+        return (this.defense * 0.4 + this.physical * 0.3 + this.passing * 0.3).toFixed(0);
+      } else if (this.position === 'Meio-Campo') {
+        return (this.shooting * 0.1 + this.passing * 0.3 + this.dribbling * 0.2 + this.defense * 0.2 + this.physical * 0.2).toFixed(0);
+      } else if (this.position === 'Meia-Atacante') {
+        return (this.shooting * 0.3 + this.dribbling * 0.3 + this.passing * 0.4).toFixed(0);
+      } else if (this.position === 'Ponta') {
+        return (this.shooting * 0.2 + this.dribbling * 0.5 + this.pace * 0.3).toFixed(0);
+      } else if (this.position === 'Atacante') {
+        return (this.shooting * 0.5 + this.dribbling * 0.2 + this.passing * 0.1 + this.physical * 0.2).toFixed(0);
+      }
     }
   }
