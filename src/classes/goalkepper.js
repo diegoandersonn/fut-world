89fc5f6b1f9721fe02480d8628c804011330394c
diegoandersonn <diegoandersoncarva@hotmail.com>
2024-09-28 12:15:31 -0300

    criada classe goalkepper

---
+++ src/classes/goalkepper.js
@@ -0,0 +1,20 @@
+export default class CreatePlayer {
+    constructor(name, team, nationality, age, number, position, overall, pace, shooting, passing, dribbling, defense, physical) {
+      this.name = name;
+      this.team = team;
+      this.nationality = nationality;
+      this.age = age;
+      this.number = number;
+      this.position = position;
+      this.overall = overall;
+      this.pace = pace;
+      this.shooting = shooting;
+      this.passing = passing;
+      this.dribbling = dribbling;
+      this.defense = defense;
+      this.physical = physical;
+    }
+    getOverall() {
+        
+    }
+  }
