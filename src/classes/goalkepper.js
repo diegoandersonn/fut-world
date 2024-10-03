5641ec011b193cdc9fa3faa3a625581d6b3128c4
Diego Anderson Carvalho <130716128+diegoandersonn@users.noreply.github.com>
2024-10-03 18:13:39 -0300

    removed getOverall function from constructor

--- src/classes/goalkepper.js
+++ src/classes/goalkepper.js
@@ -1,21 +1,12 @@
-import getOverall from "../utils/getOverall";
-
 export default class CreateGoalkepper {
-    constructor(name, team, nationality, age, number, position, diving, handling, kicking, positioning, reflexes, reactions) {
+    constructor(name, team, nationality, age, number, position, overall, diving, handling, kicking, positioning, reflexes, reactions) {
       this.name = name;
       this.team = team;
       this.nationality = nationality;
       this.age = age;
       this.number = number;
       this.position = position;
-      this.overall = getOverall(
-        this.position,  
-        this.diving, 
-        this.handling, 
-        this.kicking, 
-        this.positioning, 
-        this.reflexes, 
-        this.reactions);
+      this.overall = overall;
       this.diving = diving;
       this.handling = handling;
       this.kicking = kicking;
--- src/classes/player.js
+++ src/classes/player.js
@@ -1,22 +1,13 @@
-import getOverall from "../utils/getOverall";
-
 export default class CreatePlayer {
     id;
-    constructor(name, team, nationality, age, number, position, pace, shooting, passing, dribbling, defense, physical) {
+    constructor(name, team, nationality, age, number, position, overall, pace, shooting, passing, dribbling, defense, physical) {
       this.name = name;
       this.team = team;
       this.nationality = nationality;
       this.age = age;
       this.number = number;
       this.position = position;
-      this.overall = getOverall(
-        this.position,  
-        this.pace, 
-        this.shooting, 
-        this.passing, 
-        this.dribbling, 
-        this.defense, 
-        this.physical);
+      this.overall = overall;
       this.pace = pace;
       this.shooting = shooting;
       this.passing = passing;
